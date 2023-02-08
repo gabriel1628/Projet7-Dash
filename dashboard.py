280c06f66aa807cfffb5f05fdf5d75132c49e143
Gabriel Chehade <92156820+gabriel1628@users.noreply.github.com>
2023-02-08 16:14:15 +0100

    Update dashboard.py to use Dash

--- dashboard.py
+++ dashboard.py
@@ -1,22 +1,32 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from io import BytesIO
+from dash import Dash, html, Input, Output, dcc
+import dash_bootstrap_components as dbc
+import dash_daq as daq
 import matplotlib as mpl
-mpl.use("agg")
-# We're gonna use the Figure constructor
-# see https://matplotlib.org/stable/gallery/user_interfaces/web_application_server_sgskip.html
-# for more information
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
-
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 import requests
 
-st.set_page_config(layout="wide")
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+fontsize = '24px'
 
 
-@st.cache
+########## Import des données ##########
+
+def load_X_y(nan):
+    X = pd.read_csv('/Users/gabriel/Documents/gabriel/Git/Projet-7/data/X.csv', index_col=0).fillna(nan)
+    y = pd.read_csv('/Users/gabriel/Documents/gabriel/Git/Projet-7/data/y.csv', index_col=0)['TARGET']
+    return X, y
+
+
+nan = 1.01010101 # remplacement des NaN par cette valeur
+X, y = load_X_y(nan)
+
+index_values = X.index.tolist()
+
+
+########## Appel au modèle ##########
+
 def request_prediction(model_uri, data_json):
     headers = {"Content-Type": "application/json"}
     response = requests.request(method='POST', headers=headers, url=model_uri, json=data_json)
@@ -27,198 +37,355 @@ def request_prediction(model_uri, data_json):
 
     return response.json()
 
-
 # Adresse du modèle
 #model_uri = 'http://127.0.0.1:80/' # local
 model_uri = 'http://35.180.69.239:80/' # cloud
 
-# Chargement des data
-@st.cache(allow_output_mutation=True)
-def load_X_y(nan):
-    X = pd.read_csv('https://projet7-bucket.s3.eu-west-3.amazonaws.com/X.csv', index_col=0).fillna(nan)
-    y = pd.read_csv('https://projet7-bucket.s3.eu-west-3.amazonaws.com/y.csv', index_col=0)['TARGET']
-    return X, y
 
-nan = 1.01010101 # remplacement des NaN par cette valeur
-X, y = load_X_y(nan)
+###################
+# PREMIERE PARTIE #
+###################
 
-# First block
-left_column, middle_column, right_column = st.columns(3)
+########## Choix du client et status ##########
+left_col1 = [daq.NumericInput(
+    id='client-id',
+    # value=0,
+    min=100001,
+    max=10000000,
+    size=120,
+),
+    html.Br(),
+    html.Span('Client ', style={'font-size': fontsize}),
+    html.Span(id='client-status')]
 
-with left_column:
-    # Choix du client par son id
-    client_id = st.number_input("Client ID", key="client_id", min_value=100001, step=1)
+########## Colorbar ##########
+middle_col1 = [dcc.Graph(id='colorbar', style={'marginTop': '0em'})]
 
-if client_id > 100001:
-    data_json = {'data': X.loc[client_id].to_list(),
-                 'features_name': list(X.columns)}
+########## Status du crédit ##########
+right_col1 = [html.Span('Crédit ', style={'font-size': fontsize}),
+              html.Span(id='credit-status')]
 
-    response_json = request_prediction(model_uri, data_json)
-    #pred, global_features, global_vals, local_features, local_vals, X_imp = response_json.values()
-    X_imp, global_features, global_vals, local_features, local_vals, pred = response_json.values()
-    X_imp = pd.DataFrame(X_imp, columns=X.columns)
+########## Première ligne ##########
+row1 = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(html.Div(left_col1, style={'textAlign': 'center'})),
+                dbc.Col(html.Div(middle_col1), width=8),
+                dbc.Col(html.Div(right_col1), style={'textAlign': 'left'}),
+            ],
+            align="center",
+        ),
+    ]
+)
 
-    #       First part
+###################
+# DEUXIEME PARTIE #
+###################
 
-    with left_column:
-        # Client Status
+row2 = html.Div(
+    [
+        dbc.Row(dbc.Col(html.Div('Nombre de variables', style={'padding': 20}))),
+        dbc.Row(dbc.Col(dcc.Slider(5, 20, 1, value=10, id='n-features'), width=6)),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id='local-importance')),
+                dbc.Col(dcc.Graph(id='global-importance')),
+            ]
+        ),
+    ]
+)
+
+###################
+# TROISIEME PARTIE #
+###################
+
+########## Options du graphe ##########
+options = html.Div(
+    [
+        html.H6('Variable', style={'margin-left': 10}),
+        dcc.Dropdown(
+            X.columns.sort_values(),
+            'AMT_CREDIT',
+            id='features'
+        ),
+        html.Br(),
+        html.H6('Echelle des abscisses', style={'margin-left': 10}),
+        dcc.Dropdown(
+            ['Linéaire', 'Logarithmique'],
+            'Linéaire',
+            id='x-scale'
+        ),
+        html.Br(),
+        html.H6('Echelle des ordonnées', style={'margin-left': 10}),
+        dcc.Dropdown(
+            ['Linéaire', 'Logarithmique'],
+            'Linéaire',
+            id='y-scale'
+        ),
+    ], style={'padding': 50}
+)
+
+row3 = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(options),
+                dbc.Col(dcc.Graph(id='feature-distribution'),
+                        width=8),
+            ]
+        ),
+    ]
+)
+
+###################
+# APP LAYOUT #
+###################
+
+app.layout = html.Div(
+    [
+        dcc.Store(id='client-data'),
+        row1,
+        row2,
+        row3,
+    ]
+)
+
+
+@app.callback(
+    Output('client-status', 'children'),
+    Output('client-status', 'style'),
+    Input('client-id', 'value')
+)
+def client_status(client_id):
+    if client_id in index_values:
         if y.loc[client_id] == 0:
-            status = '<span style="color:green">Solvable.</span>'
+            status = 'Solvable'
+            style = {'color': 'green', 'font-size': fontsize, 'font-weight': 'bold'}
         else:
-            status = '<span style="color:red">Insolvable.</span>'
+            status = 'Insolvable'
+            style = {'color': 'red', 'font-size': fontsize, 'font-weight': 'bold'}
+    else:
+        status = 'Inexistant'
+        style = {'font-size': fontsize}
+    return status, style
 
-        st.write('### Statut du client : ' + status, unsafe_allow_html=True)
 
-        '#'
-        '#'
-        # Top n important features
-        n_features = st.selectbox('Nombre de variables à afficher',
-                                  range(5, 21),
-                                  index=5,
-                                  key='n_features')
+@app.callback(
+    Output('client-data', 'data'),
+    Input('client-id', 'value')
+)
+def model_prediction(client_id):
+    if client_id in index_values:
+        data_json = {'data': X.loc[client_id].to_list(),
+                     'features_name': list(X.columns)}
+        response_json = request_prediction(model_uri, data_json)
+        return response_json
 
-    with middle_column: # Colorbar
+
+@app.callback(
+    Output('colorbar', 'figure'),
+    Input('client-id', 'value'),
+    Input('client-data', 'data')
+)
+def make_colorbar(client_id, response_json):
+    if client_id in index_values:
         colors = ['r', 'darkorange', 'gold', 'limegreen', 'green']
         # définition de la barre d'échelle:
         cmap = (mpl.colors.ListedColormap(colors).with_extremes(over='0.25', under='0.75'))
+        pred = response_json['prediction']
         color = cmap(pred)
-        pred = round(pred * 100, 2)
+        rgba_tuple = tuple([0.99 if val == 1.0 else val for val in color])
+        color = 'rgba' + str(rgba_tuple)
+        pred_perc = round(pred * 100, 2)
 
-        rect1 = mpl.patches.Rectangle((0, -10),
-                                      pred, 20,
-                                      color=color,
-                                      #facecolor='w',
-                                      #edgecolor='k'
-                                      )
+        layout = go.Layout(template='simple_white', showlegend=False)
+        colorbar = go.Figure(layout=layout)
 
-        rect2 = mpl.patches.Rectangle((0, -10),
-                                      100, 20,
-                                      facecolor='none',
-                                      edgecolor='k'
-                                      )
+        # Set axes properties
+        colorbar.update_xaxes(range=[-20, 120], showgrid=False)
+        colorbar.update_yaxes(range=[-5, 40])
+        colorbar.update_xaxes(visible=False)
+        colorbar.update_yaxes(visible=False)
+
+        # Text
+        colorbar.add_trace(go.Scatter(
+            x=[50], y=[35],
+            text=str(pred_perc) + '%',
+            mode="text",
+            textfont=dict(size=18, color=color)
+        ))
+
+        colorbar.add_trace(go.Scatter(
+            x=[50], y=[5],
+            text='Probabilité de remboursement',
+            mode='text',
+            textfont=dict(size=18)
+        ))
+
+        # filled rectangle
+        colorbar.add_shape(type="rect",
+                           x0=0, y0=10, x1=pred_perc, y1=30,
+                           fillcolor=color,
+                           opacity=1,
+                           )
+
+        # Unfilled rectangle for the edge
+        colorbar.add_shape(type="rect",
+                           x0=0, y0=10, x1=100, y1=30,
+                           line=dict(
+                               color="black",
+                               width=2,
+                           ),
+                           fillcolor='rgba(0,0,0,0)',
+                           opacity=1
+                           )
+
+        colorbar.update_xaxes(visible=False)
+        colorbar.update_yaxes(visible=False)
+
+        return colorbar
+
+    else:
+        layout = go.Layout(template='simple_white', showlegend=False)
+        colorbar = go.Figure(layout=layout)
+
+        # Set axes properties
+        colorbar.update_xaxes(range=[-20, 120], showgrid=False)
+        colorbar.update_yaxes(range=[-5, 40])
+
+        colorbar.add_trace(go.Scatter(
+            x=[50], y=[5],
+            text='Probabilité de remboursement',
+            mode='text',
+            textfont=dict(size=18)
+        ))
+
+        # Unfilled rectangle for the edge
+        colorbar.add_shape(type="rect",
+                           x0=0, y0=10, x1=100, y1=30,
+                           line=dict(
+                               color="black",
+                               width=2,
+                           ),
+                           fillcolor='rgba(0,0,0,0)',
+                           opacity=1
+                           )
+
+        colorbar.update_xaxes(visible=False)
+        colorbar.update_yaxes(visible=False)
+
+        return colorbar
 
 
-        with _lock:
-            # Generate the figure **without using pyplot**.
-            fig1 = Figure()
-            ax1 = fig1.subplots()
-            ax1.text(50, 20, f'{pred}%', fontsize=20, color=color, ha='center', va='center')
-            ax1.add_patch(rect1)
-            ax1.add_patch(rect2)
-            ax1.set_xlim([-10, 110])
-            ax1.set_ylim([-30, 40])
-            ax1.axis('off')
-            ax1.set_title('Prédiction du modèle', fontsize=20, fontweight='bold')
-            ax1.text(50, -20, 'que le client soit solvable', fontsize=18, ha='center', va='center')
-            st.pyplot(fig1)
-
-
-    with right_column:
-        '# \n' * 5
-        if pred > 60:
-            result = '<span style="color:green">Accordé</span>'
+@app.callback(
+    Output('credit-status', 'children'),
+    Output('credit-status', 'style'),
+    Input('client-id', 'value'),
+    Input('client-data', 'data')
+)
+def client_status(client_id, response_json):
+    if client_id in index_values:
+        pred = response_json['prediction']
+        if pred > 0.60:
+            status = 'Accordé'
+            style = {'color': 'green', 'font-size': fontsize, 'font-weight': 'bold'}
         else:
-            result = '<span style="color:red">Refusé</span>'
-
-        st.write('### Crédit ' + result, unsafe_allow_html=True)
-
-
-    #       Feature importance
-
-    local_features, local_vals = local_features[:n_features], local_vals[:n_features]
-    global_features, global_vals = global_features[:n_features], global_vals[:n_features]
-
-    left_column_2, right_column_2 = st.columns(2)
-
-    with left_column_2: # Local importance
-        with _lock:
-            fig2 = Figure(figsize=(8, 6))
-            ax2 = fig2.subplots()
-            ax2.barh(local_features[::-1], local_vals[::-1],
-                     color=["red" if coef < 0 else "green" for coef in local_vals[::-1]])
-            # ax.set_xticks(rotation=30, horizontalalignment='right')
-            x1 = - abs(1.1 * local_vals[0])
-            x2 = - x1
-            ax2.set_xlim(x1, x2)
-            ax2.set_xlabel('Contribution')
-            ax2.set_title('Importance locale', fontsize=20, va='bottom')
-            st.pyplot(fig2)
-
-    with right_column_2: # Global importance
-        with _lock:
-            fig3 = Figure(figsize=(8, 6))
-            ax3 = fig3.subplots()
-            ax3.barh(global_features[::-1], global_vals[::-1],
-                     color=["red" if coef < 0 else "green" for coef in global_vals[::-1]])
-            x1 = - abs(1.1 * global_vals[0])
-            x2 = - x1
-            ax3.set_xlim(x1, x2)
-            ax3.set_xlabel('Contribution')
-            ax3.set_title('Importance globale', fontsize=20, va='bottom')
-            st.pyplot(fig3)
+            status = 'Refusé'
+            style = {'color': 'red', 'font-size': fontsize, 'font-weight': 'bold'}
+    else:
+        status = ' '
+        style = {'font-size': fontsize}
+    return status, style
 
 
-    #   Distribution
-    x = X.replace(nan, np.nan)
+@app.callback(
+    Output('local-importance', 'figure'),
+    Output('global-importance', 'figure'),
+    Input('client-id', 'value'),
+    Input('n-features', 'value'),
+    Input('client-data', 'data')
+)
+def feature_importance(client_id, n_features, response_json):
+    if client_id in index_values:
+        local_values = response_json['local_imp_values'][:n_features]
+        local_features = response_json['local_imp_features'][:n_features]
+        global_values = response_json['global_imp_values'][:n_features]
+        global_features = response_json['global_imp_features'][:n_features]
 
-    left_column_3, right_column_3 = st.columns(2)
-    with left_column_3:
-        feature = st.selectbox('Sélectionnez une variable',
-                               X.columns.sort_values(),
-                               key='select_features')
-        xscale = st.selectbox('Choisissez l\'échelle des abscisses',
-                             ['Linéaire', 'Logarithmique'],
-                             key='xscale_hist')
-        yscale = st.selectbox('Choisissez l\'échelle des ordonnées',
-                              ['Linéaire', 'Logarithmique'],
-                              key='yscale_hist')
-        clients = st.selectbox('Comparaison avec',
-                               ['Tous les clients', 'Clients solvables', 'Clients insolvables'],
-                               key='comparison')
-        normalize = st.checkbox('Normaliser', value=True)
+        layout = go.Layout(yaxis=dict(tickfont=dict(size=10)))
 
-        if clients == 'Tous les clients':
-            pass
-        elif clients == 'Clients solvables':
-            x = x[y == 0]
-        else:
-            x = x[y == 1]
+        local_importance = go.Figure(
+            go.Bar(
+                x=local_values[::-1],
+                y=local_features[::-1],
+                orientation='h',
+                marker_color=['red' if coef < 0 else 'green' for coef in local_values[::-1]],
+            ),
+            layout=layout
+        )
 
-    with right_column_3:
-        if x[feature].unique().size < 10:
+        global_importance = go.Figure(
+            go.Bar(
+                x=global_values[::-1],
+                y=global_features[::-1],
+                orientation='h',
+                marker_color=['red' if coef < 0 else 'green' for coef in global_values[::-1]],
+            ),
+            layout=layout
+        )
+
+    else:
+        local_importance = go.Figure()
+        global_importance = go.Figure()
+
+    return local_importance, global_importance
+
+
+@app.callback(
+    Output('feature-distribution', 'figure'),
+    Input('client-id', 'value'),
+    Input('client-data', 'data'),
+    Input('features', 'value'),
+    Input('x-scale', 'value'),
+    Input('y-scale', 'value'),
+)
+def distribution(client_id, response_json, feature, x_scale, y_scale):
+    if client_id in index_values:
+        X_imp = pd.DataFrame(response_json['X_imputed'], columns=X.columns)
+
+        log_x = False
+        if x_scale == 'Logarithmique':
+            log_x = True
+        log_y = False
+        if y_scale == 'Logarithmique':
+            log_y = True
+
+        if X[feature].unique().size < 10:
             n_bins = 10
-        if (x[feature].unique().size >= 10) & (x[feature].unique().size < 100):
-            n_bins = x[feature].unique().size
-        if (x[feature].unique().size >= 100) & (x[feature].unique().size < 1000):
+        if (X[feature].unique().size >= 10) & (X[feature].unique().size < 100):
+            n_bins = X[feature].unique().size
+        if (X[feature].unique().size >= 100) & (X[feature].unique().size < 1000):
             n_bins = 100
-        elif x[feature].unique().size > 1000:
-            n_bins = x[feature].unique().size // 10
-        n_bins = min(n_bins, 300) # no more than 300 bins
+        elif X[feature].unique().size > 1000:
+            n_bins = X[feature].unique().size // 10
+        n_bins = min(n_bins, 300)  # no more than 300 bins
 
-        with _lock:
-            fig4 = Figure(figsize=(8,6))
-            ax4 = fig4.subplots()
-            if clients == 'Tous les clients':
-                ax4.hist(x.loc[y == 0, feature], bins=n_bins, density=normalize, label='Clients solvables')
-                ax4.hist(x.loc[y == 1, feature], bins=n_bins, density=normalize, label='Clients insolvables',
-                        alpha=0.7)
-            else:
-                ax4.hist(x[feature], bins=n_bins, density=normalize)
+        fig = px.histogram(x=X[feature], color=y, histnorm='percent', nbins=n_bins,
+                           color_discrete_sequence=px.colors.qualitative.T10,
+                           labels={
+                               'x': feature,
+                               'color': 'Status'
+                           },
+                           # template='seaborn',
+                           log_x=log_x, log_y=log_y)
+        fig.add_vline(X_imp[feature].values[0], line_width=2, line_color='red')
 
-            if xscale == 'Logarithmique':
-                ax4.set_xscale('log')
-            if yscale == 'Logarithmique':
-                ax4.set_yscale('log')
+    else:
+        fig = go.Figure()
 
-            xlims = ax4.get_xlim()
-            ax4.plot(2 * [X_imp[feature]], ax4.get_ylim(), 'r', alpha=0.5, linewidth=2,
-                    label='client')
+    return fig
 
-            ax4.set_title('Distribution des valeurs', fontsize=16)
-            ax4.set_xlabel(feature)
-            ax4.legend()
-            ax4.set_xlim(xlims)
-            #st.pyplot(fig4)
-            buf = BytesIO()
-            fig4.savefig(buf, format="png")
-            st.image(buf)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
